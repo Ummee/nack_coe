ef64e7d040c6c98d11b7a160014315085c78e2c9
Mahmood Hayeelah <legendofpatriots@gmail.com>
2012-06-24 13:01:06 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NackCoe::Application.config.secret_token = '09421d6dbfab2537fba95e9fdc4df99f11b4268a378c1237b9bb60bd6dedc8947c123848679a905ec14e7ebb6fcbefb20622ff67e430cdea90e4b8dddc58d8aa'
